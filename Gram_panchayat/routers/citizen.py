2d0c4f93e3ba2ccef15846883cc8338d99d50b80
SRINJOY59 <srinjoydas566@gmail.com>
2025-03-02 01:52:36 +0530

    some government agency endpoints added

--- Gram_panchayat/routers/citizen.py
+++ Gram_panchayat/routers/citizen.py
@@ -8,7 +8,6 @@ from pydantic import BaseModel
 import logging
 from datetime import date, datetime
 
-# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
